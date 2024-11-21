b83cd1f9dd61a590f9aeb47603ff3e70fcf28353
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2024-11-22 01:55:07 +0530

    primar code

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center border-2 border-red-700 w-screen h-screen">
+      <h1 className="text-5xl text-red-200"> Hello World</h1>;
+    </div>
+  );
+};
+
+export default App;
